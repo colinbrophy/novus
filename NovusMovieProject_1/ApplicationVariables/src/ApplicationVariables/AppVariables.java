d8246dda1ddc00f3ae46c2718ab492e3138677e0
Colin Brophy <colin@brophys.com>
2017-12-22 12:41:25 +0000

    Added comments to the ApplicationVariables

--- NovusMovieProject_1/ApplicationVariables/src/ApplicationVariables/AppVariables.java
+++ NovusMovieProject_1/ApplicationVariables/src/ApplicationVariables/AppVariables.java
@@ -3,7 +3,7 @@ package ApplicationVariables;
 
 public class AppVariables {
     
-    
+    // Enum of keys for the data
     public static int filmID = 0;
     public static int filmName = 1;
     public static int imdbRating = 2;
@@ -14,20 +14,20 @@ public class AppVariables {
     public static int actorName = 6;
         
     public static class CSV{    
-        
+        // Paths to the data of the movies
         public static String FILE_PATH = "/home/colin/code/novus/NovusMovieProject_1/Data/TestData.csv";
         public static String EXTENDED_FILE_PATH = "/home/colin/code/novus/NovusMovieProject_1/Data/ExtendedTestData.csv";
     } 
       
     public static class WebProperties{
-       
+        // Strings used for the links to actors and films on IMDB
         public static String imdbFilmURL = "http://www.imdb.com/title/tt%s";
         public static String imdbProfileURL = "http://www.imdb.com/name/nm%s";
         
-        
+        // Stores the the displayed string for the inital dropdown option
         public static final String dropDownDefault = "--SELECT--";
         
-       
+        // Years in which the film are from, currently not in use
         public static String yearMin = "2000";
         public static String yearMax = "2017";
     }   
