0d4a986db3d247ca006e46355cb7d9f6c51f368b
Colin Brophy <colin@brophys.com>
2017-12-22 14:32:16 +0000

    Added comments to Beans.java

--- NovusMovieProject_1/WebMovies/src/java/PresentationLayer/Beans.java
+++ NovusMovieProject_1/WebMovies/src/java/PresentationLayer/Beans.java
@@ -14,30 +14,41 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 
-
+// Defines the lifetime of the object in this case as long as you stay on the same page
 @ViewScoped
+// Defines the name which can be used in the JavaSever faces XHTML
 @Named("bean")
 public class Beans extends BaseBean implements Serializable{
-    
+   
     private MovieBusinessLayer mbl = new MovieBusinessLayer();
+    // Strings representing the currently selected entry
     private String selectedFilm, selectedDirector, selectedActor, selectedYear, selectedRating;
+    
     List<Director> directors;
     List<Actor> actors;
     List<SimplisticFilm> sFilms;
     List<String> filmYears, filmRatings;
-    private boolean isSubmitted = false, isAllSelected = false;
+    // isSubmitted currently does nothing
+    private boolean isSubmitted = false;
+    // isAllSelected is true if there has been something selected byt the user
+    private boolean isAllSelected = false;
     
+    // Called at object construction time
     @PostConstruct
     protected void load(){
+        // Is this the initial request or a new one? 
         if (this.isPostback()){
+            // Set the IDs to the user selection
             String filmID = (selectedFilm == null ? null : selectedFilm);
             String directorID = (selectedDirector == null ? null : selectedDirector);
             String actorID = (selectedActor == null ? null : selectedActor);
             String filmYear = (selectedYear == null ? null : selectedYear);
             String filmRating = (selectedRating == null ? null : selectedRating);
-
+            
+            // Filter the options based on the current selection
             populateDropDownsWithFilteredData(filmID, directorID, actorID, filmYear, filmRating);
         }else{
+            // By default put all the distinct names as options
             populateDropDownsWithOriginalData();
         }
     }
@@ -46,7 +57,7 @@ public class Beans extends BaseBean implements Serializable{
     private void populateDropDownsWithOriginalData(){
         try{
             Films films = mbl.getFilms();
-            
+            // Get all unique options
             directors = mbl.getDistinctDirectorsFromFilms(films);
             actors = mbl.getDistinctActorsFromFilms(films);
             sFilms = mbl.getDistinctSimplisticFilmsFromFilms(films);
@@ -60,9 +71,11 @@ public class Beans extends BaseBean implements Serializable{
     //populate dropdown based on filters currently in place
     private void populateDropDownsWithFilteredData(String filmID, String directorID, String actorID, String filmYear, String filmRating){
         Films films = mbl.getFilms(); 
-            
+        
+        // Subset of films which satisfy the entered IDs
         Films tmp = mbl.getFilmsSubset(filmID, directorID, actorID, filmYear, filmRating, films);
-
+        
+        // If any of strings are null then get all otherwise filter based on the choices made
         actors = (actorID == null) ? mbl.getDistinctActorsFromFilms(tmp) : mbl.getDistinctActor(tmp, actorID);
         directors = (directorID == null) ? mbl.getDistinctDirectorsFromFilms(tmp) : mbl.getDistinctDirector(tmp, directorID);
         sFilms = (filmID == null) ? mbl.getDistinctSimplisticFilmsFromFilms(tmp) : tmp.getDistinctSimplisticFilm(filmID);
@@ -72,6 +85,7 @@ public class Beans extends BaseBean implements Serializable{
         //display table of data once all values are selected
         if(sFilms.size() == 1 && directors.size() == 1 && actors.size() == 1 && filmYears.size() == 1){
             isAllSelected = true;
+            // Put the first film in the list in
             this.populateFields(sFilms.get(0).filmID, directors.get(0).personID, actors.get(0).personID);
         }
     }
@@ -86,7 +100,9 @@ public class Beans extends BaseBean implements Serializable{
     
     //http://ruleoftech.com/2012/jsf-1-2-and-getting-selected-value-from-dropdown
     public void filmValueChanged(ValueChangeEvent e){
+        // Is this the initial request or a new one? 
         if(isPostback()){
+            // Reload the table based on the new value
             selectedFilm = e.getNewValue().toString();
             this.load();
         }
@@ -156,7 +172,7 @@ public class Beans extends BaseBean implements Serializable{
     private Film film;
     private Director director;
     private Actor actor;
-    
+    // Put the fields in the webpage for JSF
     public void populateFields(String filmID, String directorID, String actorID){
         this.film = mbl.getFilmFromSimplisticFilm(filmID);
         this.director = mbl.getDirectorFromSimplisticFilm(film, directorID);
@@ -164,6 +180,7 @@ public class Beans extends BaseBean implements Serializable{
     }
     
     //JSF read access to fields
+    // JSF will call these functions
     public Film getFilm(){return film;}
     public Director getDirector(){return director;}
     public Actor getActor(){return actor;}
